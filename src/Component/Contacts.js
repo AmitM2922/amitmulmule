3b0282e2db7cbe9ac5d9c7c4305fd670d578ffc9
Amit Mulmule <68117653+AmitM2922@users.noreply.github.com>
2022-08-17 18:09:35 +0530

    Update Contacts.js

--- src/Component/Contacts.js
+++ src/Component/Contacts.js
@@ -1,33 +1,24 @@
 import React from "react";
+import axios from 'axios'
 import { useState } from "react";
-import emailjs from "emailjs-com";
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [result, setResult] = useState(false);
-  function sendEmail(e){
-    e.preventDefault();
-    emailjs.sendForm("service_zsveawb",
-      "tempelate_v4soljn",
-      e.target,
-      'uaFVWjWHpy0_1VxTZ')
-      .then((r) => {
-          console.log(r.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-      setResult(true);
-      setInterval(() => {
-        setResult(false);
-      }, 5000);
-  }
-  const Success=()=>{
-    return(
-      <p className="text-green-400 text-center ">Your message has been successfully sent .</p>
-    )
-  }
+    const [msg,setMsg] = useState('');
+    const [user, setUser] = useState({
+      to: "",
+      Name: "",
+      Message: ""
+    });
+   
+    const { to, Name, Message} = user;
+    const onInputChange = e => {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    };
+   
+    const onSubmit = async e => {
+      e.preventDefault();
+      await axios.post("http://localhost:5000/users/",user)
+     .then(response => setMsg(response.data.respMesg));
+    };
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -68,7 +59,7 @@ export default function Contact() {
           </div>
         </div>
         <form
-        onSubmit={sendEmail}
+        
           name="contact"        
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -86,7 +77,8 @@ export default function Contact() {
               id="name"
               name="from_name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-             
+              onChange={onInputChange}
+              value={Name}
             />
           </div>
           <div className="relative mb-4">
@@ -94,6 +86,8 @@ export default function Contact() {
               Email
             </label>
             <input
+            onChange={onInputChange}
+            value={to}
               type="email"
               id="email"
               name="email"
@@ -108,21 +102,19 @@ export default function Contact() {
               Message
             </label>
             <textarea
+             onChange={onInputChange}
+             value={Message}
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"             
             />
           </div>
           <button
-
+          onClick={onSubmit}
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
-          </button>
-
-         {
-          result ?<Success/>:null
-         }
+          </button>{msg}
         </form>
       </div>
     </section>
