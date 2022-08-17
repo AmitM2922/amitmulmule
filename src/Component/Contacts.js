5804d7e0db17564a4bf91ef15e5a6bad3837c9db
Amit Mulmule <68117653+AmitM2922@users.noreply.github.com>
2022-08-17 14:10:40 +0530

    update

--- src/Component/Contacts.js
+++ src/Component/Contacts.js
@@ -1,9 +1,30 @@
 import React from "react";
-
+import { useState } from "react";
+import emailjs from "emailjs-com";
 export default function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [result, setResult] = useState(false);
+  function sendEmail(e){
+    e.preventDefault();
+    emailjs.sendForm('service_zsveawb', 'tempelate_v4soljn',e.target, 'uaFVWjWHpy0_1VxTZ')
+      .then((r) => {
+          console.log(r.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+      setResult(true);
+      setInterval(() => {
+        setResult(false);
+      }, 5000);
+  }
+  const Success=()=>{
+    return(
+      <p className="text-green-400 text-center ">Your message has been successfully sent .</p>
+    )
+  }
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -44,6 +65,7 @@ export default function Contact() {
           </div>
         </div>
         <form
+        onSubmit={sendEmail}
           name="contact"        
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -59,7 +81,7 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="name"
+              name="fullname"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
              
             />
@@ -85,16 +107,19 @@ export default function Contact() {
             <textarea
               id="message"
               name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-             
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"             
             />
           </div>
           <button
-            
+
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
+
+         {
+          result ?<Success/>:null
+         }
         </form>
       </div>
     </section>
