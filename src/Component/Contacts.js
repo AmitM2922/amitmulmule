9f1b3234116bd35cf07085a808c54d58e099d8c9
Amit Mulmule <68117653+AmitM2922@users.noreply.github.com>
2022-08-17 14:52:56 +0530

    Update Contacts.js

--- src/Component/Contacts.js
+++ src/Component/Contacts.js
@@ -81,7 +81,7 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="fullname"
+              name="from_name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
              
             />
