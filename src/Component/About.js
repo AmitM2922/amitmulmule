060d25c5b777316c8c292c973f4edbf7cd1b7d35
Amit Mulmule <68117653+AmitM2922@users.noreply.github.com>
2022-08-17 11:46:36 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -6,14 +6,14 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Amit.
+            Hi, I'm Amit Mulmule.
             <br className="hidden lg:inline-block" />I love to build amazing
             web apps.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          A motivated individual with in-depth knowledge of languages and development tools, seeking a position
+          in a growth-oriented company where I can use my skills to the advantage of the company while having
+          the scope to develop my own skills.
           </p>
           <div className="flex justify-center">
             <a
